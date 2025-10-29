90f2e0fa5bc0e6b21f73ea1608ca77199964b65e
God'swill Eguavoen <godswilleguavoen@gmail.com>
2025-10-29 11:51:05 +0100

    made sure that the user cant be logged in as another user by using their url and if he or she tries it will log them out and say unauthorised

--- Classes/Accounts/auth_page.php
+++ Classes/Accounts/auth_page.php
@@ -3,10 +3,10 @@
 
 function requireRole($role){
     if(!isset($_SESSION["account"])){
-      header("Location: ../../signup.login.php?error=notloggedin");
+      header("Location: ../../../signup.login.php?error=notloggedin");
       exit();
     }elseif($_SESSION["account"] !== $role){
-      header("Location: ../../signup.login.php?error=unauthorized");
+      header("Location: ../../../signup.login.php?error=unauthorized");
       exit();
     }
 
